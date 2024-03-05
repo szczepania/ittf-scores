2712853bbb8ebd428133fbf7b60897d0b283c1f9
szczepania <szczepanikanja@gmail.com>
2024-03-05 17:02:32 +0100

    Counting percentage of men and women in ranking

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,18 @@
+import analysis.GenderPercentageCalculator._
+import utils.FileUtils.readFiles
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main extends App {
 
-  val spark = SparkSession.builder().master("local[*]").getOrCreate()
+  val playerInfoDF = readFiles("data/csv/ittf_player_info.csv")
 
-  def readFiles(file: String): DataFrame = {
-    spark
-      .read
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .csv(file)
-  }
+  val genderColumn = "gender"
+  val man = "Male"
+  val woman = "Female"
 
+  val womenPercentage = calculateGenderPercentage(playerInfoDF, genderColumn, woman)
+  val menPercentage = calculateGenderPercentage(playerInfoDF, genderColumn, man)
+
+  println(s"Percentage of female is $womenPercentage")
+  println(s"Percentage of male is $menPercentage")
 }
